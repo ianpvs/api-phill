ca404e192a9b5a01b93b2a52d03efa79ea122d37
Gabriel Soares <gabrielsoaresm94@gmail.com>
2019-06-29 22:23:39 -0300

    postToPhill

--- src/controllers/PhillController.js
+++ src/controllers/PhillController.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const Phill = require('../models/phill');
 
 module.exports = {
@@ -21,5 +22,13 @@ module.exports = {
         const id = req.params.id;
         const data = await Phill.find({ user_id: id }).sort('-createdAt');
         return res.json(data);
+    },
+
+    async postToPhill(req, res) {
+        const { sintomas, user_id } = req.body;
+        const result = await axios.post('http://localhost:5000/output', { sintomas, user_id })
+        console.log(result.data);
+        return res.status(200);
+        
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,8 @@ const routes = new express.Router();
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
 routes.get('/phill', PhillController.index);
-routes.post('/phill', PhillController.store);
+//routes.post('/phill', PhillController.store);
+routes.post('/phill', PhillController.postToPhill);
 routes.get('/phill/:id', PhillController.show);
 
 module.exports = routes;
